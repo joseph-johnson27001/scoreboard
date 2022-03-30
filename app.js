1bb6145525e30ff5b2d5c308566de8b31c8d21d6
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2022-03-30 15:10:26 +0700

    Used props to make dynamic info

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const Header = () => {
+const Header = (props) => {
     return (
         <header>
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
+            <h1>{props.title}</h1>
+            <span className="stats">Players: {props.totalPlayers}</span>
         </header>
     );
 }
 
-const Player = () => {
+const Player = (props) => {
     return (
         <div className="player">
             <span className="player-name">
-                Joe
+                {props.name}
             </span>
-            <Counter />
+            <Counter score={35}/>
         </div>
     )
 }
 
-const Counter = () => {
+const Counter = (props) => {
     return (
         <div className="counter">
             <button className="counter-action decrement"> - </button>
-            <span className="counter-score"> 35 </span>
+            <span className="counter-score"> {props.score} </span>
             <button className="counter-action increment"> + </button>
         </div>
     );
@@ -31,10 +31,12 @@ const Counter = () => {
 const App = () => {
     return (
         <div className="scoreboard">
-            <Header />
+            <Header title="Scoreboard"
+                totalPlayers={1}
+            />
 
             {/* Start of players list */}
-            <Player />
+            <Player name="Joe"/>
         </div>
     )
 }
