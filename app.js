c2ac212a6375fbe4f0d4cd3fcfdb47948e3756b2
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2022-03-31 13:35:47 +0700

    Increment and decrement buttons functionality

--- app.js
+++ app.js
@@ -42,19 +42,28 @@ const Player = (props) => {
 }
 
 class Counter extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            score: 0
-        };
+    state = {
+        score: 0
+    };
+    
+    incrementScore = () => {
+        this.setState({
+            score: this.state.score + 1
+        });
+    }
+
+    decrementScore = () => {
+        this.setState({
+            score: this.state.score - 1
+        });
     }
 
     render() {
         return (
             <div className="counter">
-                <button className="counter-action decrement"> - </button>
+                <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
                 <span className="counter-score"> { this.state.score } </span>
-                <button className="counter-action increment"> + </button>
+                <button className="counter-action increment" onClick={this.incrementScore}> + </button>
             </div>
         );
     }
